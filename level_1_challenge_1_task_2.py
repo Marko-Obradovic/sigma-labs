4dc4de906e2e3a85e4310d64085550ed58286c6b
marko-obradovic <marko@obradovic.co.uk>
2024-12-17 15:17:15 +0000

    level 1 challenge 1 task 2 fix

--- level_1_challenge_1_task_2.py
+++ level_1_challenge_1_task_2.py
@@ -1,20 +1,22 @@
-def get_name():
-    name = input("What's your first name?: ")
+'''
+A program that asks the user for their name and either:
+
+1. Greets them with their name in the console if authorised
+2. Rejects authorisation if name is not authorised'''
+
+def get_name() -> str:
+    name: str = input("What's your first name?: ")
     return name.title().strip()
 
 
-def get_greeting(name):
+def get_greeting(name: str) -> str:
     if name in {"Alice", "Bob"}:
-        return(f"Hello {name}!")
-    else:
-        return("Sorry... You're not authorised to be greeted!")
+        return f"Hello {name}!"
+    return "Sorry... You're not authorised to be greeted!"
 
 
-def main(name):
-    greeting_string = name
-    print(greeting_string)
+def main(greeting: str) -> str:
+    print(greeting)
 
 if __name__ == "__main__":
     main(get_greeting(get_name()))
-
-
