6eda3f79ac4fbb644580e919d975a58c494ad268
marko-obradovic <marko@obradovic.co.uk>
2024-12-17 16:11:18 +0000

    cleaning up level 1 challenge 1 task 2

--- level_1_challenge_1_task_2.py
+++ level_1_challenge_1_task_2.py
@@ -1,22 +1,23 @@
 '''
-A program that asks the user for their name and either:
+This program checks if the user's name is in the list of authorised names and greets them if it is.
 
-1. Greets them with their name in the console if authorised
-2. Rejects authorisation if name is not authorised'''
+It will prompt the user for their first name and respond accordingly'''
+
+authorised_names = {"Alice", "Bob"}
 
 def get_name() -> str:
     name: str = input("What's your first name?: ")
     return name.title().strip()
 
 
-def get_greeting(name: str) -> str:
-    if name in {"Alice", "Bob"}:
+def greet_if_authorised(name: str, authorised_names_list: set) -> str:
+    if name in authorised_names_list:
         return f"Hello {name}!"
     return "Sorry... You're not authorised to be greeted!"
 
 
-def main(greeting: str) -> str:
-    print(greeting)
+def main(message: str) -> None:
+    print(message)
 
 if __name__ == "__main__":
-    main(get_greeting(get_name()))
+    main(greet_if_authorised(get_name(), authorised_names))
