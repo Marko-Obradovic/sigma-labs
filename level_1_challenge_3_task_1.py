794429e112c8021e5accb277dc83d28862cf38a4
marko-obradovic <marko@obradovic.co.uk>
2024-12-18 00:22:06 +0000

    level 1 challenge 3 task 1 refactor

--- level_1_challenge_3_task_1.py
+++ level_1_challenge_3_task_1.py
@@ -1,45 +1,55 @@
+"""This program takes a number of your choice, either sums or multiplies the numbers 
+from 1 to your number - Sums the numbers from 3 to your number in multiples of 3 and 5.
+"""
+
+import sys
+
+import functools
+from typing import Callable, Any
+
 import styling
 
-print(
-    '\nThis program does the following:\n\n- Takes a number of your choice\n- Either sums or multiplies the numbers from 1 to your number\n- Sums the numbers from 3 to your number in multiples of 3 and 5.\n'
-)
+def error_handle_get_number_input(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except KeyboardInterrupt:
+                styling.notification('Program stopped')
+                sys.exit()
+            except ValueError:
+                print(error_message)
+    return wrapper
+
+@error_handle_get_number_input
+def get_number_input(error_message):
+    return int(input('Enter a number: '))
 
 
-def ask_user_for_number(error_message):
-    while True:
-        try:
-            return int(input('Enter a number: '))
-            break
-        except ValueError:
-            print(error_message)
-        except KeyboardInterrupt:
-            styling.notification('Program stopped')
-            quit()
-
-
-def sum_to_n(end_value_parameter) -> int:
-    total = 0
+def sum_to_n(end_value_parameter: int) -> int:
+    total: int = 0
     for num in range(1, end_value_parameter + 1):
         total += num
     return total
 
 
-def multiply_to_n(end_value_parameter) -> int:
-    total = 1
+def multiply_to_n(end_value_parameter: int) -> int:
+    total: int = 1
     for num in range(1, end_value_parameter + 1):
         total *= num
     return total
 
 
-def generate_num_sequence(end_value_parameter, incrementation) -> list:
+def generate_num_sequence(end_value_parameter: int, incrementation:int) -> list:
     return [
         num for num in range(0, end_value_parameter + 1, incrementation)
         if num > 0
     ]
 
 
-def sum_num_sequences(sequence_one, sequence_two) -> int:
-    total = 0
+def sum_num_sequences(sequence_one: list[int], sequence_two: list[int]) -> int:
+    total: int = 0
     for num in sequence_one + sequence_two:
         total += num
     return total
@@ -48,31 +58,42 @@ def sum_num_sequences(sequence_one, sequence_two) -> int:
 operator_choice_error = styling.notification('Error. Please enter either 1 or 2')
 invalid_number_error = styling.notification('Error. Please enter a valid number')
 
-end_value = ask_user_for_number(error_message=invalid_number_error)
+end_value = get_number_input(error_message=invalid_number_error)
 
 print(styling.line_separator)
 
-print(
-    '\nChoose an operation to perform on numbers up to your selected number:\n\n1. Sum the numbers\n2. Multiply the numbers\n'
-)
+def main() -> None:
+    print(
+        '\nChoose an operation to perform on numbers up to your selected number:\n\n1. Sum the numbers\n2. Multiply the numbers\n'
+    )
 
-while True:
-    operator_choice = ask_user_for_number(error_message=operator_choice_error)
-    if operator_choice == 1:
-        print(styling.line_separator)
-        print(
-            f'\nNumbers from 1 to {end_value} summed = {sum_to_n(end_value)}\n'
-        )
-        break
-    if operator_choice == 2:
-        print(styling.line_separator)
-        print(
-            f'\nNumbers from 1 to {end_value} multiplied = {multiply_to_n(end_value)}\n'
-        )
-        break
-    else:
+    while True:
+        operator_choice = get_number_input(error_message=operator_choice_error)
+        if operator_choice == 1:
+            print(styling.line_separator)
+            print(
+                f'\nNumbers from 1 to {end_value} summed = {sum_to_n(end_value)}\n'
+            )
+            break
+        if operator_choice == 2:
+            print(styling.line_separator)
+            print(
+                f'\nNumbers from 1 to {end_value} multiplied = {multiply_to_n(end_value)}\n'
+            )
+            break
         print(operator_choice_error)
+ 
+    multiples_of_three = generate_num_sequence(end_value, incrementation = 3)
+    multiples_of_five = generate_num_sequence(end_value, incrementation = 5)
 
-print(
-    f'Multiples of three or five up to your chosen number summed: {sum_num_sequences(generate_num_sequence(end_value, incrementation = 3), generate_num_sequence(end_value, incrementation = 5))}\n'
-)
+    multiples_of_three_or_five_summed = sum_num_sequences(
+            multiples_of_three,
+            multiples_of_five
+            )
+
+    multiples_of_three_or_five_message = f'Multiples of three or five up to your chosen number summed: {multiples_of_three_or_five_summed}'
+
+    print(multiples_of_three_or_five_message)
+
+if __name__ == "__main__":
+    main()
