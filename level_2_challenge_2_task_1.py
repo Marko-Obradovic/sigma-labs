1c12147924f050854934085bd2337a37e606fed4
marko-obradovic <marko@obradovic.co.uk>
2024-12-26 19:45:37 +0000

    level_2_challenge_2_task_1.py refactored

--- level_2_challenge_2_task_1.py
+++ level_2_challenge_2_task_1.py
@@ -1,3 +1,12 @@
+"""This program is a number-guessing game.
+
+In this game, the user tries to guess a randomly generated secret 
+number between 0 and 100. 
+
+The user will be prompted to enter a guess and will receive feedback 
+on whether to guess higher or lower until they find the correct number.
+"""
+
 import sys
 
 import random
@@ -6,46 +15,55 @@ from collections.abc import Callable
 
 import styling
 
-
-
-def error_handling(func: Callable) -> Callable:
+def error_handling(func: Callable[[], int]) -> Callable[[], int]:
     @functools.wraps(func)
-    def wrapper() -> :
+    def wrapper() -> int:
         while True:
             try:
                 return func()
             except KeyboardInterrupt:
-                print("\n\nProgram stopped.")
+                print("")
+                print(styling.notification("Program Stopped"))
                 sys.exit()
             except ValueError:
                 print(styling.notification("Invalid input. Please enter a valid number."))
                 continue
     return wrapper
 
+
 @error_handling
-def user_guess() -> int:
+def prompt_guess() -> int:
     return int(input("Guess a number between 0 and 100: "))
 
-def main() -> None:
-    answer: int = random.randint(0, 100) 
-    prev_guess: int = 0
+
+def check_guess() -> str:
+    answer: int = random.randint(0, 100)
+    previous_guess: int = 0
     tries: int = 0
-    guess: int = 0
-    while guess != answer:
-        guess: int = user_guess()
-        tries += 1
-        if guess == prev_guess:
-            print('\nSame as previous answer. Continuing...\n')
+
+    while True:
+        guess: int = prompt_guess()
+        if guess == previous_guess:
+            print(styling.notification("Same as previous answer. Continuing..."))
             continue
-        elif guess < answer:
-            print('\nHigher...\n')
+        elif guess < 0 or guess > 100:
+            print(styling.notification("Please enter a value between 0 and 100"))
+            continue
+        tries += 1
+        if guess < answer:
+            print("Higher...")
         elif guess > answer:
-            print('\nLower...\n')
+            print("Lower...")
         elif guess == answer:
-            print('\nYou Won!\n')
+            print("You Won!")
             break
-        prev_guess: int = guess
-    print(f"Attempts: {tries}")
+        previous_guess = guess
+
+    return f"Attempts: {tries}"
+
+
+def main() -> None:
+    print(check_guess())
 
 if __name__ == '__main__':
     main()
